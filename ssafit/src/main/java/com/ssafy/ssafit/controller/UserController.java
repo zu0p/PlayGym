b88cf5bd10dedd94502d08a1a162d10be7ad9c89
mooonmovie <sosm8500@gmail.com>
2021-10-25 13:53:17 +0900

    Feat 회원가입   exception처리 및 ResponseEntity 처리

--- ssafit/src/main/java/com/ssafy/ssafit/controller/UserController.java
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/UserController.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,15 +35,28 @@ public class UserController {
 	private final MainuserRepository userRepository;
 	
 	@PostMapping("/join")
-	public Long join(@RequestBody Map<String, String> user) {
-		return userRepository.save(MainUser.builder()
-                .userId(user.get("userid"))
-				.email(user.get("email"))
-                .password(passwordEncoder.encode(user.get("password")))
-                .name(user.get("name"))
-                .phone(user.get("phone"))
-                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
-                .build()).getId();
+	public ResponseEntity<Map<String,Object>> join(@RequestBody Map<String, String> user) {
+		Map<String,Object> ret = new HashMap<String, Object>();
+		try {
+			userRepository.save(MainUser.builder()
+	                .userId(user.get("userid"))
+					.email(user.get("email"))
+	                .password(passwordEncoder.encode(user.get("password")))
+	                .name(user.get("name"))
+	                .phone(user.get("phone"))
+	                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+	                .build()).getId();
+		} catch(ConstraintViolationException e) {
+			ret.put("result","속성 값에 null이 존재합니다.");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ret);
+		}catch (DataIntegrityViolationException e) {
+			ret.put("result","이미 있는 아이디 입니다.");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ret);
+		} 
+		ret.put("result","회원가입이 완료되었습니다.");
+		return ResponseEntity.status(HttpStatus.OK).body(ret);
+		
+		
 	}
 	
 	 // 로그인
