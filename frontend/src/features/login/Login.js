05f411147b07f57913e417b9fe4bd435b7b50b5e
박주영 <a33a66a99@naver.com>
2021-10-25 18:01:56 +0900

    Fix password validation

--- frontend/src/features/login/Login.js
+++ frontend/src/features/login/Login.js
@@ -6,15 +6,43 @@ import TextField from '@mui/material/TextField';
 export function Login() {
   const [id, setId] = useState('')
   const [pwd, setPwd] = useState('')
+  const [errorText, setErrorText] = useState('')
   const handleIdChange=(e)=>{
     setId(e.target.value)
   }
   const handlePwdChange=(e)=>{
-    setPwd(e.target.value)
-  }
-  const validation=()=>{
+    const input = e.target.value
+    setPwd(input)
 
+    if(input.length<8 || input.length>16){
+      setErrorText('8~16자 사이로 입력해 주세요')
+      return
+    }
+    
+    const eng = input.search(/[a-zA-Z]/g)
+    const num = input.search(/[0-9]/gi)
+    const spec = input.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi)
+    if(num<0 || eng<0 || spec<0){
+      setErrorText('영문, 숫자, 특수문자를 포함해 주세요')
+      return
+    }
+
+    setErrorText('')
   }
+
+  const validation=()=>{
+    if(pwd.length<9 || pwd.length>16)
+      return true
+    
+    const eng = pwd.search(/[a-zA-Z]/g)
+    const num = pwd.search(/[0-9]/gi)
+    const spec = pwd.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi)
+    if(num<0 || eng<0 || spec<0)
+      return true
+
+    return false
+  }
+
   return (
     <div>
       <h1>PlayGym</h1>
@@ -29,8 +57,8 @@ export function Login() {
       />
       <TextField
         required
-        error={validation}
-        helperText={validation?"text":""}
+        error={validation()}
+        helperText={errorText}
         id="outlined-required"
         label="PW"
         type="password"
