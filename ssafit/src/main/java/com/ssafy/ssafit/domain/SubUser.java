286406335267382a9f253d425de43878dc6381d4
Jeongdong <duwjdehd0713@gmail.com>
2021-10-26 15:23:49 +0900

    Feat SubUser Entity 일부 수정

--- ssafit/src/main/java/com/ssafy/ssafit/domain/SubUser.java
+++ ssafit/src/main/java/com/ssafy/ssafit/domain/SubUser.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssafit.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,17 +9,21 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @ToString
+@Builder
 public class SubUser {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "INT UNSIGNED")
 	private long sid; // 기본키
 	private String nickName;
 	
@@ -32,5 +37,23 @@ public class SubUser {
 	private int weight;
 	private long cid;
 
+	@Builder
+	public SubUser(String nickName, MainUser mainUser, int age, int tall, int weight, int cid) {
+		this.nickName = nickName;
+		this.mainUser = mainUser;
+		this.age = age;
+		this.tall = tall;
+		this.weight = weight;
+		this.cid = cid;
+	}
+	
+	public void update(String nickName, MainUser mainUser, int age, int tall, int weight) {
+		this.nickName = nickName;
+		this.mainUser = mainUser;
+		this.age = age;
+		this.tall = tall;
+		this.weight = weight;
+	}
+	
 }
 
--- ssafit/src/main/java/com/ssafy/ssafit/repository/SubuserRepository.java
+++ ssafit/src/main/java/com/ssafy/ssafit/repository/SubuserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ssafit.domain.MainUser;
+import com.ssafy.ssafit.domain.SubUser;
 
-public interface SubuserRepository extends JpaRepository<MainUser, Long>{
+public interface SubuserRepository extends JpaRepository<SubUser, Long>{
 	
+	Optional<SubUser> findByMainUser(MainUser mainuser);
 }
