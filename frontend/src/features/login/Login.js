83f59f989d35501ba725a1b882060057d00d5c3c
박주영 <zu0p@kakao.com>
2021-10-27 13:31:04 +0900

    Fix modify style and css

--- frontend/src/features/login/Login.js
+++ frontend/src/features/login/Login.js
@@ -3,40 +3,10 @@ import { Route, Link, NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { requestLoginUser } from '../../app/actions/userActions';
 import logo from '../../images/play_gym_logo.png';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-import styles from './Login.css';
+import styles from './Login.module.css';
+import {SubmitButton, LoginTextField} from './styledComponent';
 
-import { styled } from '@mui/material/styles';
-const SubmitButton = styled(Button)({
-  boxShadow: 'none',
-  textTransform: 'none',
-  fontSize: 16,
-  padding: '6px 12px',
-  border: '3px solid',
-  lineHeight: 1.5,
-  backgroundColor: '#FFFFFF',
-  borderColor: '#A3C653',
-  color: '#22220B',
-  width: '200px',
-  height: '50px',
-  '&:hover': {
-    backgroundColor: '#A3C653',
-    borderColor: '#A3C653',
-    color: '#FFFFFF',
-    boxShadow: 'none',
-  },
-  '&:active': {
-    boxShadow: 'none',
-    backgroundColor: '#A3C653',
-    borderColor: '#A3C653',
-    color: '#FFFFFF',
-  },
-  '&:focus': {
-    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-  },
-});
 
 export function Login() {
   const dispatch = useDispatch()
@@ -125,20 +95,20 @@ export function Login() {
   }
 
   return (
-    <div className={styles.login_container} style={{background:'#D5E8A6', height: '100vh'}}>
+    <div className={styles.login_container} >
       {/* <h1>{ id }, { pw }</h1> */}
       <Grid
         container
         direction="column"
         justifyContent="center"
         alignItems="center"
-        spacing={2}
+        spacing={4}
       >
         <Grid item mt={"20%"} mb={5}>
           <img src={logo} width="150"/>
         </Grid>
         <Grid item>
-          <TextField
+          <LoginTextField
             required
             error={ idValidation() }
             helperText={ idError }
@@ -149,7 +119,7 @@ export function Login() {
           />
         </Grid>
         <Grid item>
-          <TextField
+          <LoginTextField
             required
             error={ pwValidation() }
             helperText={ pwError }
