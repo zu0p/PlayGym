c9d440cee21a7833e4bffd943392787430d8d09b
박주영 <zu0p@kakao.com>
2021-10-29 10:37:12 +0900

    Feat create login api connection

--- frontend/src/app/actions/userActions.js
+++ frontend/src/app/actions/userActions.js
@@ -1,10 +1,10 @@
 import { LOGIN_USER } from './types'
 import { request } from '../../utils/axios'
 
-const USER_URL = 'api/user'
+const USER_URL = ''
 
 export function requestLoginUser(userInfo){
-  const returnData = request('post', USER_URL+'login', userInfo).then(res=>{console.log(res)})
+  const returnData = request('post', USER_URL+'/login', userInfo)
   return {
     type: LOGIN_USER,
     payload: returnData
