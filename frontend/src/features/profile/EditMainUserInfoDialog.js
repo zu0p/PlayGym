bf509aedde6519345bb548fd4288354067a678b3
박주영 <zu0p@kakao.com>
2021-11-02 11:46:20 +0900

    Feat connect api - update main user info, signout user

--- frontend/src/features/profile/EditMainUserInfoDialog.js
+++ frontend/src/features/profile/EditMainUserInfoDialog.js
@@ -7,8 +7,11 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
+import Collapse from '@mui/material/Collapse';
+import Alert from '@mui/material/Alert';
 import Slide from '@mui/material/Slide';
 import Grid from '@mui/material/Grid'
+import styles from './Profile.module.css'
 import { FullDialogBar, FullDialogGrid, AddTextField } from './customProfileStyle'
 import { requestUnsignupUser, requestUpdateUser } from '../../app/actions/userActions'
 
@@ -27,15 +30,18 @@ export default function EditMainUserInfoDialog(props) {
   const [email, setEmail] = useState(userState.mainUser.email)
   const [phone, setPhone] = useState(userState.mainUser.phone)
 
+  const [signout, setSignout] = useState(false)
+
   const handleClose = () => {
     props.getClose(true)
+    setSignout(false)
+    setInputDisable(true)
   }
 
   const onEditClick = () => {
     if(inputDisable){
       console.log('before')
       setInputDisable(false)
-      console.log(document.getElementById('editButton'))
       const eButton = document.getElementById('editButton')
       eButton.style.backgroundColor='#7DA3D4'
       eButton.innerText='save'
@@ -46,8 +52,14 @@ export default function EditMainUserInfoDialog(props) {
       oButton.style.marginRight = '10px'
     }
     else{
-      console.log('save')
+      // console.log('save')
       setInputDisable(true)
+      const eButton = document.getElementById('editButton')
+      eButton.style.backgroundColor='#A3C653'
+      eButton.innerText='edit'
+
+      const oButton = document.getElementById('outButton')
+      oButton.style.display = 'none'
 
       // action: modify main user info
       let userInfo = {
@@ -60,18 +72,30 @@ export default function EditMainUserInfoDialog(props) {
       dispatch(
         requestUpdateUser(userInfo)
       )
+
       // close dialog
+      // handleClose()
     }
   }
 
   const onOutClick = () => {
+    console.log(signout)
+    setSignout(true)
+    console.log(signout)
+  }
+
+  const onSignout = () => {
+
     console.log('회원탈퇴')
     // action: unsign up
     // console.log(userState.mainUser.id)
     dispatch(
       requestUnsignupUser(userState.mainUser.id)
     )
-    // close dialog
+    
+    // back to login page
+    localStorage.removeItem('access-token')
+    window.location.href = '/'
   }
 
   const handleChangeName = (e) => {
@@ -98,6 +122,30 @@ export default function EditMainUserInfoDialog(props) {
         onClose={handleClose}
         TransitionComponent={Transition}
       >
+        <Collapse in={signout} >
+          <Alert
+            className={styles.outAlert}
+            severity="error"
+            action={
+              <div>
+              <Button onClick={onSignout} color="error">탈퇴</Button>
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setSignout(false)
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+              </div>
+            }
+            sx={{ mb: 2 }}
+          >
+            회원 탈퇴하시면 모든 정보가 삭제 됩니다. 정말로 회원 탈퇴하시겠습니까?
+          </Alert>
+        </Collapse>
         <FullDialogBar sx={{ position: 'relative' }}>
           <Toolbar>
             <IconButton
