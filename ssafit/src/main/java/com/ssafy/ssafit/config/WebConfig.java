e8e1ce209ff5df50fe2b915baa1803d6727724c5
mooonmovie <sosm8500@gmail.com>
2021-11-11 11:17:19 +0900

    Fix Security config 수정

--- ssafit/src/main/java/com/ssafy/ssafit/config/WebConfig.java
+++ ssafit/src/main/java/com/ssafy/ssafit/config/WebConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("*")
-				.allowedHeaders("*")
-				.exposedHeaders("Authorization");
-
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//				.allowedOrigins("*")
+//				.allowedMethods("*")
+//				.allowedHeaders("*")
+//				.exposedHeaders("Authorization");
+//
+//	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- ssafit/src/main/java/com/ssafy/ssafit/config/WebSecurityConfig.java
+++ ssafit/src/main/java/com/ssafy/ssafit/config/WebSecurityConfig.java
@@ -11,13 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.ssafy.ssafit.security.CorsFilter;
 import com.ssafy.ssafit.security.JwtAuthenticationFilter;
 import com.ssafy.ssafit.security.JwtTokenProvider;
 
@@ -47,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
-				.cors().configurationSource(corsConfigurationSource())
+				.cors()
 				.and()
 				.csrf().disable() // csrf 보안 토큰 disable처리.
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지
@@ -56,28 +54,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
 						UsernamePasswordAuthenticationFilter.class).authorizeRequests()// 요청에 대한 사용권한 체크
 				
-				.antMatchers(HttpMethod.OPTIONS,"/user/**").hasRole("USER")
+				.antMatchers("/user/**").hasRole("USER")
 				.anyRequest().permitAll(); // 그외 나머지 요청은 누구나 접근 가능
 	
 		// JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
 	}
 	
 
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-    	
-    	CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-//        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    	
+//	.configurationSource(corsConfigurationSource())
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//    	
+//    	CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.addAllowedOrigin("*");
+//        configuration.addAllowedHeader("*");
+//        configuration.addAllowedMethod("*");
+////        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    	
     	
 //        CorsConfiguration configuration = new CorsConfiguration();
 //        configuration.setAllowedOrigins(Arrays.asList("*"));
@@ -87,5 +85,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
-    }
+//    }
 }
