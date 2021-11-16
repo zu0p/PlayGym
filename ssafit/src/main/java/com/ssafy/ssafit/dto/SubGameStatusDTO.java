59de5d83a75c13949b06bdab7f75026e642f2222
mooonmovie <sosm8500@gmail.com>
2021-11-16 11:12:32 +0900

    Fix use/sub/status req 수정

--- ssafit/src/main/java/com/ssafy/ssafit/dto/SubGameStatusDTO.java
+++ ssafit/src/main/java/com/ssafy/ssafit/dto/SubGameStatusDTO.java
@@ -12,13 +12,15 @@ public class SubGameStatusDTO {
 	private String img;
 	private long sid;
 	private String name;
+	private int max;
 	
-	public SubGameStatusDTO(int exp, int kcal,String img,long sid,String name) {
+	public SubGameStatusDTO(int exp, int kcal,String img,long sid,String name,int max) {
 		this.exp=exp;
 		this.kcal=kcal;
 		this.img=img;
 		this.sid = sid;
 		this.name = name;
+		this.max = max;
 	}
 
 }
